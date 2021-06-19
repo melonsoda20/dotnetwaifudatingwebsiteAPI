43abe2cb9cd441c2a4afed349d504aeba82c351b
melonsoda20 <benedictusteddy200596@gmail.com>
2021-06-19 17:17:43 +0700

    Add login feature in Account controller

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -43,6 +43,28 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDTO loginDTO){
+            AppUser user = await _context.Users
+                .SingleOrDefaultAsync(user => user.UserName == loginDTO.Username);
+
+            if(user == null){
+                return Unauthorized("Invalid username");
+            }
+
+            using HMACSHA512 hmac = new HMACSHA512(user.PasswordSalt);
+
+            byte[] computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+
+            for(int i = 0; i < computedHash.Length; i++){
+                if(computedHash[i] != user.PasswordHash[i]){
+                    return Unauthorized("Invalid password");
+                }
+            }
+
+            return user;
+        }
+
         private async Task<bool> UserExists(string username){
             return await _context.Users.AnyAsync(user => user.UserName == username.ToLower());
         }
