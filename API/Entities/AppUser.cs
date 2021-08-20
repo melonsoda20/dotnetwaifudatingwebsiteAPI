58e82b4069b1a6c5fa373ebdba8a6b02809ca956
melonsoda20 <benedictusteddy200596@gmail.com>
2021-08-20 19:02:28 +0700

    Add new date extension to calculate age

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using API.Extensions;
 
 namespace API.Entities
 {
@@ -9,7 +10,7 @@ namespace API.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash {get; set;}
         public byte[] PasswordSalt { get; set; }
-        public DateTime DateOfBirht { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
         public DateTime LastActive { get; set; } = DateTime.Now;
@@ -20,5 +21,10 @@ namespace API.Entities
         public string City {get; set;}
         public string Country {get; set;}
         public ICollection<Photo> Photos {get; set;}
+
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalculateAge(this DateTime dob)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - dob.Year;
+            if(dob.Date > today.AddYears(-age))
+            {
+                age--;
+            }
+            return age;
+        }
+    }
+}
