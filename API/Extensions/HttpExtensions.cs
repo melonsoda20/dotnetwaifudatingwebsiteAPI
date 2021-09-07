d0ee61c05fb69181c12be9a3cf6bbe746b414292
melonsoda20 <benedictusteddy200596@gmail.com>
2021-09-07 16:29:23 +0700

    Add filter by gender to the API

--- API/Extensions/HttpExtensions.cs
+++ API/Extensions/HttpExtensions.cs
@@ -20,7 +20,7 @@ namespace API.Extensions
             };
 
             response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader, options));
-            response.Headers.Add("Access-Control-Expose-Headers", "Paginations");
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
         }
     }
 }
